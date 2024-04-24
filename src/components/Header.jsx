8018b7eb1d7c378196c6f1ebe05a29622c941e63
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-24 04:18:41 -0400

    Added links to navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,7 +75,7 @@ const Header = () => {
          {/* <img className="h-14 w-14" src={Image} alt="Example" /> */}
           <div className="text-[#787878] text-lg font-thin">&lt;/</div>
           <div className="origin-center text-[#64ffda] text-2xl font-light">J</div>
-          <div className="text-[#787878] tracking-wider text-2xl font-thin flex flex-row"><div className="text-[#bed2fc] font-light">C</div>&gt;</div>
+          <div className="text-[#787878] tracking-wider text-2xl font-thin flex flex-row"><div className="text-[#bed2fc] font-thin">C</div>&gt;</div>
       </button>
       
       <nav className="w-full h-full">
@@ -102,11 +102,11 @@ const Header = () => {
           <br className="h-20"></br>
           <br className="h-20"></br>
           <div className='flex flex-row justify-end items-center space-x-8'>
-            <button>
+            <button onClick={()=>window.open('https://github.com/JabirC', '_blank')}>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="none" stroke="#bdc5de" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
               </svg>
             </button>
-            <button>
+            <button onClick={()=>window.open('https://www.linkedin.com/in/jabirc/', '_blank')}>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#bdc5de" fillRule="evenodd" d="M9.429 8.969h3.714v1.85c.535-1.064 1.907-2.02 3.968-2.02c3.951 0 4.889 2.118 4.889 6.004V22h-4v-6.312c0-2.213-.535-3.461-1.897-3.461c-1.889 0-2.674 1.345-2.674 3.46V22h-4zM2.57 21.83h4V8.799h-4zM7.143 4.55a2.53 2.53 0 0 1-.753 1.802a2.573 2.573 0 0 1-1.82.748a2.59 2.59 0 0 1-1.818-.747A2.548 2.548 0 0 1 2 4.55c0-.677.27-1.325.753-1.803A2.583 2.583 0 0 1 4.571 2c.682 0 1.336.269 1.819.747c.482.478.753 1.126.753 1.803" clipRule="evenodd"></path>
               </svg>
             </button>
