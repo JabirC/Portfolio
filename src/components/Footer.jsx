a9623369c720b7b6ee24ce392fcfa46271360225
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-24 04:10:32 -0400

    Complete Work Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,13 +18,13 @@ const Footer = () => {
     <>
     <div className="z-1 md:fixed flex flex-col justify-center items-center pt-10 space-y-6 md:space-y-4 md:pb-10 md:pb-0 bottom-0 left-0 w-full md:w-20 lg:w-24 xl:w-32 h-24 md:h-400">
         <ul className="flex flex-row md:flex-col justify-center items-center space-x-8  md:space-x-0 md:space-y-8">
-            <li className="flex flex-col md:flex-row justify-center items-center">
+            <li className="flex flex-col md:flex-row justify-center items-center" onClick={()=>window.open('https://github.com/JabirC', '_blank')}>
                 <a className="transition-transform transform hover:-translate-y-1" onMouseEnter={()=>setColorGitHub("#64ffda")} onMouseLeave={()=>setColorGitHub('#8892b0')} href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="none" stroke={colorGitHub} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
                     </svg>
                 </a>
             </li>
-            <li className="flex flex-col md:flex-row justify-center items-center">
+            <li className="flex flex-col md:flex-row justify-center items-center" onClick={()=>window.open('https://www.linkedin.com/in/jabirc/', '_blank')}>
                 <a className="transition-transform transform hover:-translate-y-1" onMouseEnter={()=>setColorLinkedIn("#64ffda")} onMouseLeave={()=>setColorLinkedIn('#8892b0')} href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill={colorLinkedIn} fillRule="evenodd" d="M9.429 8.969h3.714v1.85c.535-1.064 1.907-2.02 3.968-2.02c3.951 0 4.889 2.118 4.889 6.004V22h-4v-6.312c0-2.213-.535-3.461-1.897-3.461c-1.889 0-2.674 1.345-2.674 3.46V22h-4zM2.57 21.83h4V8.799h-4zM7.143 4.55a2.53 2.53 0 0 1-.753 1.802a2.573 2.573 0 0 1-1.82.748a2.59 2.59 0 0 1-1.818-.747A2.548 2.548 0 0 1 2 4.55c0-.677.27-1.325.753-1.803A2.583 2.583 0 0 1 4.571 2c.682 0 1.336.269 1.819.747c.482.478.753 1.126.753 1.803" clipRule="evenodd"></path>
                     </svg>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,34 +9,124 @@ export default function Work() {
                  <div className="poppins-custom text-xl md:text-2xl sm:text-2xl text-[#bed2fc] font-medium">  Projects</div>
                 </div>
 
-                <div className="flex w-full sm:flex-row flex-col sm:space-x-4 sm:space-y-0 space-y-4 pt-10"> 
+                <div className="poppins-custom flex w-full sm:flex-row flex-col sm:space-x-4 sm:space-y-0 space-y-4 pt-10"> 
                     <div className="flex flex-col space-y-4 w-full">
-                        <div className="h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2">
+
+                        <div className="h-96 lg:h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2 hover:cursor-pointer" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}>
+
                             <div className="absolute top-8 left-6"> 
-                                <button className="h-6 w-6">
+                                <button className="h-6 w-6" onClick={()=>window.open('https://github.com/JabirC/AdventChess', '_blank')}>
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 16 16" fill="none" stroke="#8892b0" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]" strokeWidth="1.5"><path d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
                                     </svg>
                                 </button>
                             </div>
-                            <div className="absolute top-8 right-6"> 
+                            <div className="absolute top-8 right-6" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}> 
                                 <button className="h-6 w-6">
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img"  viewBox="0 0 24 24" fill="none" stroke="#8892b0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
                                 </button>
                             </div>
-
-                            
+                            <div className="relative top-20 pl-6 pr-6 text-xl text-[#ccd6f6] font-bold">
+                                AdventChess
+                            </div>
+                            <div className="relative top-24 pl-6 pr-6 text-sm text-[#8892b0] font-normal">
+                                A multiplayer networked Chess web application that seeks to redefine the strategic nature of the game by incorporating randomized intial start states.
+                            </div>
+                            <div className="flex-wrap absolute bottom-10 pl-6 pr-6 text-xs text-[#8892b0] font-normal flex flex-row items-start">
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Java</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Spring Boot</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Angular</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                            </div>
                         </div>
-                        <div className="h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2">
 
+                        <div className="h-96 lg:h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2 hover:cursor-pointer" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}>
+
+                            <div className="absolute top-8 left-6"> 
+                                <button className="h-6 w-6" onClick={()=>window.open('https://github.com/JabirC/AdventChess', '_blank')}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 16 16" fill="none" stroke="#8892b0" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]" strokeWidth="1.5"><path d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="absolute top-8 right-6" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}> 
+                                <button className="h-6 w-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img"  viewBox="0 0 24 24" fill="none" stroke="#8892b0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                </button>
+                            </div>
+                            <div className="relative top-20 pl-6 pr-6 text-xl text-[#ccd6f6] font-bold">
+                                AdventChess
+                            </div>
+                            <div className="relative top-24 pl-6 pr-6 text-sm text-[#8892b0] font-normal">
+                                A multiplayer networked Chess web application that seeks to redefine the strategic nature of the game by incorporating randomized intial start states.
+                            </div>
+                            <div className="flex-wrap absolute bottom-10 pl-6 pr-6 text-xs text-[#8892b0] font-normal flex flex-row items-start">
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Java</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Spring Boot</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Angular</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                            </div>
                         </div>
+
                     </div>
                     <div className="flex flex-col space-y-4 w-full">
-                        <div className="h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2">
 
-                        </div>
-                        <div className="h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2">
+                        <div className="h-96 lg:h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2 hover:cursor-pointer" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}>
 
+                            <div className="absolute top-8 left-6"> 
+                                <button className="h-6 w-6" onClick={()=>window.open('https://github.com/JabirC/AdventChess', '_blank')}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 16 16" fill="none" stroke="#8892b0" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]" strokeWidth="1.5"><path d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="absolute top-8 right-6" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}> 
+                                <button className="h-6 w-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img"  viewBox="0 0 24 24" fill="none" stroke="#8892b0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                </button>
+                            </div>
+                            <div className="relative top-20 pl-6 pr-6 text-xl text-[#ccd6f6] font-bold">
+                                AdventChess
+                            </div>
+                            <div className="relative top-24 pl-6 pr-6 text-sm text-[#8892b0] font-normal">
+                                A multiplayer networked Chess web application that seeks to redefine the strategic nature of the game by incorporating randomized intial start states.
+                            </div>
+                            <div className="flex-wrap absolute bottom-10 pl-6 pr-6 text-xs text-[#8892b0] font-normal flex flex-row items-start">
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Java</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Spring Boot</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Angular</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                            </div>
                         </div>
+
+                        <div className="h-96 lg:h-80 w-full bg-[#112240] rounded-md shadow-lg transition-transform transform hover:-translate-y-2 hover:cursor-pointer" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}>
+
+                            <div className="absolute top-8 left-6"> 
+                                <button className="h-6 w-6" onClick={()=>window.open('https://github.com/JabirC/AdventChess', '_blank')}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 16 16" fill="none" stroke="#8892b0" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]" strokeWidth="1.5"><path d="M5.75 14.25s-.5-2 .5-3c0 0-2 0-3.5-1.5s-1-4.5 0-5.5c-.5-1.5.5-2.5.5-2.5s1.5 0 2.5 1c1-.5 3.5-.5 4.5 0c1-1 2.5-1 2.5-1s1 1 .5 2.5c1 1 1.5 4 0 5.5s-3.5 1.5-3.5 1.5c1 1 .5 3 .5 3m-5-.5c-1.5.5-3-.5-3.5-1"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="absolute top-8 right-6" onClick={()=>window.open('https://advent-chess.vercel.app', '_blank')}> 
+                                <button className="h-6 w-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img"  viewBox="0 0 24 24" fill="none" stroke="#8892b0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link hover:stroke-[#64ffda]"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                </button>
+                            </div>
+                            <div className="relative top-20 pl-6 pr-6 text-xl text-[#ccd6f6] font-bold">
+                                AdventChess
+                            </div>
+                            <div className="relative top-24 pl-6 pr-6 text-sm text-[#8892b0] font-normal">
+                                A multiplayer networked Chess web application that seeks to redefine the strategic nature of the game by incorporating randomized intial start states.
+                            </div>
+                            <div className="flex-wrap absolute bottom-10 pl-6 pr-6 text-xs text-[#8892b0] font-normal flex flex-row items-start">
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Java</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Spring Boot</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">Angular</div>
+                                <div className="border p-1 border-[#64ffda] text-[#64ffda] flex-shrink-0 m-1">JUnit</div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
           </div>
